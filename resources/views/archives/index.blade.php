7b05da872741f0162f715d4deb7acb1440084b37
kanouao <67881390+kanouao-room@users.noreply.github.com>
2022-05-05 20:47:51 +0900

    フロントの文字などを表示まで。

--- resources/views/archives/index.blade.php
+++ resources/views/archives/index.blade.php
@@ -8,8 +8,15 @@
     <title>記事一覧</title>
     <style>
         body{
-            background-color:red;
+            /* width: 800px; */
+            /* background-color:white; */
             text-align:center;
+            /* background-image: url(images/image2.jpg); */
+            /* background-repeat: no-repeat; */
+            /* background-size: contain; */
+            /* background-position:center; */
+            font-family: Arial, "ヒラギノ角ゴ Pro W3", "Hiragino Kaku Gothic Pro", Osaka, メイリオ, Meiryo, "ＭＳ Ｐゴシック", "MS PGothic", sans-serif;
+             font-size: 20px;
         }
         .text1{
             margin:30px 30px 30px 30px;
@@ -19,25 +26,38 @@
         width: 300px;
         height: 200px;
         }
-
+        button{
+            width: 300px;
+            height:100px;
+            background-color:#FFEFD5;
+            font-size: 20px;
+        }
+        form{
+            margin:30px 30px 100px 30px;
+            
         }
     </style>
 </head>
 <body>
     <h1>灯油入れた日記録アプリ</h1>
 
-<div>
-  <select class="same-width-list">
-    <option value="1">担当者A</option>
-    <option value="2">担当者B</option>
-    <option value="3">担当者C</option>    
-    <select/>
-</div>
 
+ <form>
+  <select class="same-width-list">
+    <option value="1" name="question" value="担当者A">担当者A</option>
+    <option value="2" name="question" value="担当者B">担当者B</option>
+    <option value="3" name="question" value="担当者C">担当者C</option>    
+    <select/>
+
+<p> 作業した日付を入力して下さい。</p>
+<p><input type="text"></p>
 
 
     <p class="text1">灯油を入れたら下の登録ボタンを押して下さい。</p>
-    
+    <p><button type="submit">登録する</button>
+
+    </form>    
+    <p>過去記録一覧</p>
 
 </body>
 </html>
